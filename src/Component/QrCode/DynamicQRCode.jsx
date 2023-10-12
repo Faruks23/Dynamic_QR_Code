9db7fb97bf98acd152ac8f802a83894409e0ec9d
Faruks23 <mdfaruksp2@gmail.com>
2023-10-12 15:09:12 +0600

    custiomiz function

--- src/Component/QrCode/DynamicQRCode.jsx
+++ src/Component/QrCode/DynamicQRCode.jsx
@@ -3,45 +3,49 @@ import QRCode from "qrcode.react";
 
 const DynamicQRCode = () => {
   const [data, setData] = useState("initial_data"); // Initial data for the QR code
-  const [exp,setExp]=useState(false)
-  const [expirationTime, setExpirationTime] = useState(Date.now()+ 1000); // 5 minutes in milliseconds
-   const minutes = Math.floor((expirationTime / (1000 * 60)) % 60);
-    console.log(minutes);
-  // Update the QR code data when the component mounts and every 10 seconds
+  const [exp, setExp] = useState(false);
+  const [expirationTime, setExpirationTime] = useState(100); // 5 minutes in seconds
+  const [intervalId, setIntervalId] = useState(null);
+
+  // Function to convert seconds to a time string
+  const secondsToTime = (seconds) => {
+    if (seconds < 0) {
+      return "Expired";
+    }
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes} minutes, ${remainingSeconds} seconds`;
+  };
+
+  // Update the QR code data when the component mounts and every second
   useEffect(() => {
     // Function to update the QR code data
     const updateQRCode = () => {
-      // Update data to be a combination of your unique identifier and the expiration time
-      setData(`your_unique_identifier|${expirationTime}`);
+      if (expirationTime > 0) {
+        setExpirationTime((prev) => prev - 1);
+      } else {
+        // The QR code has expired
+        setExp(true);
+        clearInterval(intervalId);
+        console.log("QR Code Expired");
+      }
     };
 
-    // Initial update
-    updateQRCode();
-
-    // Set an interval to update the QR code data every 10 seconds
-    const intervalId = setInterval(updateQRCode, 1000); // 10 seconds in milliseconds
+    // Set an interval to update the QR code data every second
+    const id = setInterval(updateQRCode, 1000); // 1 second in milliseconds
+    setIntervalId(id);
 
     // Clear the interval when the component unmounts
     return () => clearInterval(intervalId);
-  }, [expirationTime]); // Re-run effect when expirationTime changes
-
-  // Check if the QR code has expired
-  useEffect(() => {
-    
-    if (Date.now()> expirationTime) {
-      // The QR code has expired, you can take action here
-      console.log("QR Code Expired");
-      setExp(true);
-    }
-
   }, [expirationTime]);
 
   return (
     <div>
       <h2>Dynamic QR Code with 5-Minute Time Limit</h2>
+      <p>Remaining Time: {secondsToTime(expirationTime)}</p>
       {exp ? (
         <>
-          <div className="">Qr code expiration time limit</div>
+          <div className="">QR code has expired</div>
         </>
       ) : (
         <>
